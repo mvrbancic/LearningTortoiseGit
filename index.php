0070cae7d4fc2607d1a958554f5c26dcb3812132
mvrbancic <marko.vrba@gmail.com>
2016-02-14 15:36:54 +0100

    Add nav to index.php

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Title of the document</title>
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>Title of the document</title>
+	</head>
 
-<body>
-Content of the document......
-</body>
+	<body>
+		<header>
+			<h1>Git training</h1>
+		</header>
+		<nav>
+			<ul>
+				<?php
+					$nav = array( '0' => 'BLog', '1'=>'About', '2' => 'Archives', '3' => 'Contact');
+					for($i = 0; $i<count($nav); $i++)
+					{
+						echo '<li><a href="#">'.$nav[$i].'</a></li>';
+					}
+				?>
+			</ul>
+		</nav>
+		<section id="intro">
+		
+		</section>
+		<section>
+		
+		</section>
+		<aside>
+		
+		</aside>
+		<footer>
+		
+		</footer>
+	</body>
 
 </html> 
