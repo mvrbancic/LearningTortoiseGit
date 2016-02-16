2b4f5f896951af231b0c67acb44623bcd81b4ed3
dario <dfilkovi@gmail.com>
2016-02-16 16:54:46 +0100

    asxdf

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,5 +1,5 @@
 function myFunction() {
     var x = document.getElementById("demo");
-    x.style.fontSize = "256px";           
+    x.style.fontSize = "2px";           
     x.style.color = "red"; 
 }
